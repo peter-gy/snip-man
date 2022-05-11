fc387a230ed442554e435264d1763eb8350eb4f9
Péter Ferenc Gyarmati <dev.petergy@gmail.com>
2022-05-11 15:12:02 +0200

    feat: create base interface for entities

--- libs/entities/src/lib/entities.ts
+++ libs/entities/src/lib/entities.ts
@@ -1,3 +1,31 @@
-export function entities(): string {
-  return 'entities';
+export interface BaseEntity {
+  id: string;
+}
+
+export interface IUser extends BaseEntity {
+  username: string;
+  password: string;
+  email: string;
+}
+
+export interface IProgTopic extends BaseEntity {
+  name: string;
+  description: string;
+}
+
+export interface ITag extends BaseEntity {
+  name: string;
+  color: string;
+}
+
+export interface IProgSnippet extends BaseEntity {
+  headline: string;
+  content: string;
+  lastModified: Date;
+  createdAt: Date;
+}
+
+export interface IProgLanguage extends BaseEntity {
+  name: string;
+  version: string;
 }
