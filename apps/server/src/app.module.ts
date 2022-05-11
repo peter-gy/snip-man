4d2df74056e15bf3c4896ede89ae7aa045a536f3
Péter Ferenc Gyarmati <dev.petergy@gmail.com>
2022-05-11 20:52:51 +0200

    refactor: move `prisma` module into `libs`

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -3,8 +3,8 @@ import { ConfigModule } from '@nestjs/config';
 
 import { configuration } from './lib/config/configuration';
 import { validationSchema } from './lib/config/validation';
-import { PrismaMongoModule } from './prisma/prisma-mongo/prisma-mongo.module';
-import { PrismaPostgresModule } from './prisma/prisma-postgres/prisma-postgres.module';
+import { PrismaMongoModule } from './lib/prisma/prisma-mongo/prisma-mongo.module';
+import { PrismaPostgresModule } from './lib/prisma/prisma-postgres/prisma-postgres.module';
 
 @Module({
   imports: [
