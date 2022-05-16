b4fadc7ac1b23e770d1fe16e385ec41fa22a100e
Péter Ferenc Gyarmati <dev.petergy@gmail.com>
2022-05-16 22:49:58 +0200

    chore: add prisma SQL logging

--- apps/server/src/frameworks/data-services/mongo/prisma-mongo.service.ts
+++ apps/server/src/frameworks/data-services/mongo/prisma-mongo.service.ts
@@ -1,6 +1,13 @@
-import { Injectable } from '@nestjs/common';
-import { PrismaClient as PrismaMongoClient } from '@prisma/mongo-client';
-import { ConfigService } from '@nestjs/config';
+import { Injectable, Logger } from "@nestjs/common";
+import { PrismaClient as PrismaMongoClient } from "@prisma/mongo-client";
+import { ConfigService } from "@nestjs/config";
+
+// Source: https://www.prisma.io/docs/reference/api-reference/prisma-client-reference#event-types
+type QueryEvent = {
+  timestamp: Date
+  query: string // Query sent to the database
+  target: string
+}
 
 @Injectable()
 /**
@@ -15,9 +22,18 @@ export class PrismaMongoService extends PrismaMongoClient {
     super({
       datasources: {
         db: {
-          url: config.get('DATABASE_URL_MONGO'),
-        },
+          url: config.get("DATABASE_URL_MONGO")
+        }
       },
+      log: [{
+        emit: "event",
+        level: "query"
+      }]
+    });
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    this.$on("query", (event: QueryEvent) => {
+      Logger.debug(`Query: ${event.query}`);
     });
   }
 }
