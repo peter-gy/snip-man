d868764c9a1e528da13acd3ea97802d75c9caa79
Péter Ferenc Gyarmati <dev.petergy@gmail.com>
2022-05-16 23:15:10 +0200

    feat: basic CRUD for `ProgTopicEntity`

--- apps/server/src/frameworks/data-services/postgres/repository-impl/prog-topic.repository.ts
+++ apps/server/src/frameworks/data-services/postgres/repository-impl/prog-topic.repository.ts
@@ -8,48 +8,41 @@ export class ProgTopicRepository implements IBaseRepository<ProgTopicEntity> {
   constructor(private readonly prisma: PrismaPostgresService) {}
 
   create(item: Partial<ProgTopicEntity>): Promise<ProgTopicEntity> {
-    return this.prisma.progTopic
-      .create({
-        data: {
-          userId: item.userId,
-          parentId: item.parentId,
-          name: item.name,
-          description: item.description,
-        },
-      })
-      .then((res) => ({ ...res, tags: [], progSnippets: [] }));
+    return this.prisma.progTopic.create({
+      data: {
+        userId: item.userId,
+        parentId: item.parentId,
+        name: item.name,
+        description: item.description,
+      },
+    });
   }
 
   findUnique<A extends keyof ProgTopicEntity>(
     by: keyof ProgTopicEntity,
     attribute: Pick<ProgTopicEntity, A>
   ): Promise<ProgTopicEntity | null> {
-    return this.prisma.progTopic
-      .findUnique({
-        where: { [by]: attribute },
-        include: {
-          tags: { select: { tag: true } },
-        },
-      })
-      .then((res) => ({ ...res, tags: res.tags.map((t) => t.tag) }));
+    return this.prisma.progTopic.findUnique({
+      where: { [by]: attribute },
+    });
   }
 
   findAll(): Promise<ProgTopicEntity[]> {
-    return this.prisma.progTopic
-      .findMany({
-        include: {
-          tags: { select: { tag: true } },
-        },
-      })
-      .then((res) =>
-        res.map((t) => ({ ...t, tags: t.tags.map((t) => t.tag) }))
-      );
+    return this.prisma.progTopic.findMany();
   }
 
   update(
     id: Pick<ProgTopicEntity, 'id'>,
     item: Partial<ProgTopicEntity>
   ): Promise<ProgTopicEntity> {
-    return Promise.resolve(undefined);
+    return this.prisma.progTopic.update({
+      where: { id: id as unknown as string },
+      data: {
+        userId: item.userId,
+        parentId: item.parentId,
+        name: item.name,
+        description: item.description,
+      },
+    });
   }
 }
