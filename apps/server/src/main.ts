b39558dc5ebaa43aa311c83045adc1e8a599fca3
Péter Ferenc Gyarmati <dev.petergy@gmail.com>
2022-06-04 16:01:33 +0200

    style: format code

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
-import { Logger, ValidationPipe } from "@nestjs/common";
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { join } from 'path';
 import { config as dotenvConfig } from 'dotenv';
