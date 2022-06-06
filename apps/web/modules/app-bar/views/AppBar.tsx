1aa2138b4a104b5fad9a1e9b4e32aab417ba7892
Péter Ferenc Gyarmati <dev.petergy@gmail.com>
2022-06-06 09:45:22 +0200

    feat: add app bar

---
+++ apps/web/modules/app-bar/views/AppBar.tsx
@@ -0,0 +1,45 @@
+import { TbHome } from 'react-icons/tb';
+import Link from 'next/link';
+import { Path } from '../../routes/types/route.type';
+import { Avatar, Tooltip } from '@geist-ui/core';
+import { useSnipManState } from '../../snip-man-state/context/SnipManContext';
+
+function AppBar() {
+  const {
+    state: { user },
+  } = useSnipManState();
+  return (
+    <div className="bg-navy-800">
+      <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+        <div className="flex items-center justify-between h-16">
+          <Link href={Path.Home} passHref>
+            <div className="flex items-center space-x-3 text-4xl text-white font-bold cursor-pointer">
+              <TbHome />
+              <div>
+                Snip<span className="text-highlight">Man</span>
+              </div>
+            </div>
+          </Link>
+          {user && (<UserAvatar username={user.username} email={user.email} />)}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+type UserAvatarProps = {
+  username: string;
+  email: string;
+};
+
+function UserAvatar({ username, email }: UserAvatarProps) {
+  return (
+    <div className="text-lg font-bold">
+      <Tooltip text={email} placement="bottom">
+        <Avatar text={username.charAt(0).toUpperCase()} isSquare={true} width={1.25} height={1.25} />
+      </Tooltip>
+    </div>
+  );
+}
+
+export default AppBar;
--- apps/web/pages/app/index.tsx
+++ apps/web/pages/app/index.tsx
@@ -1,7 +1,12 @@
 import { NextPage } from 'next';
+import AppBar from '../../modules/app-bar/views/AppBar';
 
 const AppIndex: NextPage = () => {
-  return <h1>Actual Application</h1>;
+  return (
+    <div className="h-screen bg-blue-100">
+      <AppBar />
+    </div>
+  );
 };
 
 export default AppIndex;
