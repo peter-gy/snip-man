ded3700f41d0a31a455c54211d17154d2562d280
Péter Ferenc Gyarmati <dev.petergy@gmail.com>
2022-06-07 16:37:29 +0200

    feat: enhance report UI

--- apps/web/modules/reports/language-users/views/LanguageUsersReportCard.tsx
+++ apps/web/modules/reports/language-users/views/LanguageUsersReportCard.tsx
@@ -3,10 +3,8 @@ import { BsCodeSquare } from 'react-icons/bs';
 import { Developer } from '../../../layout/views/Footer';
 import { Loading, Modal, useModal } from '@geist-ui/core';
 import ProgLanguageSelector from '../../../prog-language-selector/views/ProgLanguageSelector';
-import { useState } from 'react';
 import { ProgLanguageEntity } from '@snip-man/entities';
 import useLanguageUsersReport from '../hooks/useLanguageUsersReport';
-import { data } from 'autoprefixer';
 
 const developer = {
   name: 'Péter Ferenc Gyarmati',
@@ -27,10 +25,6 @@ const description =
 
 function LanguageUsersReportCard() {
   const { setVisible: setModalVisible, bindings: modalBindings } = useModal();
-  const [didSubmit, setDidSubmit] = useState(false);
-  const [selectedLang, setSelectedLang] = useState<
-    ProgLanguageEntity | undefined
-  >(undefined);
 
   const {
     mutate: getLanguageUsersReport,
@@ -38,13 +32,10 @@ function LanguageUsersReportCard() {
     isLoading,
   } = useLanguageUsersReport();
 
-  async function onSubmit() {
-    setDidSubmit(true);
+  async function onLanguageSelected(selectedLang: ProgLanguageEntity) {
     if (!selectedLang) return;
     await getLanguageUsersReport(selectedLang.id);
   }
-
-  const warningClass = 'font-bold text-red-600 underline';
 
   return (
     <>
@@ -58,29 +49,50 @@ function LanguageUsersReportCard() {
           description={description}
         />
       </div>
-      <Modal {...modalBindings} width={3} height={40}>
+      <Modal {...modalBindings} width={3} height={45}>
         <Modal.Title>{title}</Modal.Title>
         <Modal.Subtitle>Actionable Insights ⚡️</Modal.Subtitle>
         <Modal.Content>
           <div className="flex flex-col justify-center items-center space-y-6">
             <p className="text-justify">{description}</p>
             <div className="flex flex-col items-center justify-center">
-              <h4 className={didSubmit && !selectedLang ? warningClass : ''}>
-                Please choose a language
-              </h4>
-              <ProgLanguageSelector
-                initialValue={selectedLang}
-                onChange={setSelectedLang}
-              />
+              <h4>Please choose a language</h4>
+              <ProgLanguageSelector onChange={onLanguageSelected} />
             </div>
             {isLoading && <Loading>Loading</Loading>}
-            {queryResult?.data && <p>{JSON.stringify(queryResult?.data)}</p>}
+            {queryResult?.data && (
+              <>
+                {
+                  <div className="p-4 border-dashed border-black border-2 rounded-md w-full flex justify-center items-start h-[30vh]">
+                    {queryResult?.data.length === 0 && (
+                      <p className="font-medium">No results were found</p>
+                    )}
+                    {queryResult?.data.length !== 0 && (
+                      <div className="flex flex-col">
+                        <p className="font-medium">
+                          The following email addresses were found:
+                        </p>
+                        <div className="max-h-[20vh] overflow-scroll flex flex-col space-y-1.5">
+                          {[...queryResult.data, ''].map((email) => (
+                            <div
+                              key={email}
+                              className="border-gray-300 border-t-[0.5px] my-1 italic"
+                            >
+                              {email}
+                            </div>
+                          ))}
+                        </div>
+                      </div>
+                    )}
+                  </div>
+                }
+              </>
+            )}
           </div>
         </Modal.Content>
-        <Modal.Action passive onClick={() => setModalVisible(false)}>
-          Back
+        <Modal.Action onClick={() => setModalVisible(false)}>
+          <span className="font-bold">Done</span>
         </Modal.Action>
-        <Modal.Action onClick={onSubmit}>Submit</Modal.Action>
       </Modal>
     </>
   );
