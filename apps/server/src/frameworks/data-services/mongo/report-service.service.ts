79b5ed781d0738a84101e0d46d71f28203110ef5
Péter Ferenc Gyarmati <dev.petergy@gmail.com>
2022-06-12 12:37:28 +0200

    feat: implement mongo query
    no optimizations

--- apps/server/src/frameworks/data-services/mongo/report-service.service.ts
+++ apps/server/src/frameworks/data-services/mongo/report-service.service.ts
@@ -14,10 +14,93 @@ export class ReportService implements IReportService {
     throw NotImplementedException;
   }
 
-  findUsersActiveInSpecificLanguage(
+  async findUsersActiveInSpecificLanguage(
     progLanguage: Partial<ProgLanguageEntity>
   ): Promise<string[]> {
-    console.log('mongo - findUsersActiveInSpecificLanguage');
-    throw NotImplementedException;
+    const progLanguageName = progLanguage.name as string;
+    const result = await this.prisma.user.aggregateRaw({
+      pipeline: [
+        {
+          $project: {
+            email: 1,
+          },
+        },
+        {
+          $lookup: {
+            from: 'prog_topic',
+            localField: '_id',
+            foreignField: 'user_id',
+            as: 'prog_topic',
+          },
+        },
+        {
+          $unwind: '$prog_topic',
+        },
+        {
+          $project: {
+            email: 1,
+            prog_topic_id: '$prog_topic._id',
+          },
+        },
+        {
+          $lookup: {
+            from: 'prog_snippet',
+            localField: 'prog_topic_id',
+            foreignField: 'prog_topic_id',
+            as: 'prog_snippet',
+          },
+        },
+        {
+          $unwind: '$prog_snippet',
+        },
+        {
+          $project: {
+            email: 1,
+            prog_snippet_language_name: '$prog_snippet.prog_language.name',
+            prog_snippet_created_at: {
+              $convert: { input: '$prog_snippet.created_at', to: 'string' },
+            },
+          },
+        },
+        {
+          $match: {
+            $expr: {
+              $and: [
+                { $eq: ['$prog_snippet_language_name', progLanguageName] },
+                {
+                  $gte: [
+                    '$prog_snippet_created_at',
+                    new Date(
+                      new Date().getTime() - 1000 * 86400 * 30
+                    ).toISOString(),
+                  ],
+                },
+                {
+                  $lte: ['$prog_snippet_created_at', new Date().toISOString()],
+                },
+              ],
+            },
+          },
+        },
+        {
+          $group: {
+            _id: '$email',
+            count: { $sum: 1 },
+          },
+        },
+        {
+          $match: {
+            count: { $gte: 3 },
+          },
+        },
+        {
+          $project: {
+            _id: 1,
+          },
+        },
+      ],
+    });
+    const castedResult = result as unknown as { _id: string }[];
+    return castedResult.map(({ _id: email }) => email);
   }
 }
