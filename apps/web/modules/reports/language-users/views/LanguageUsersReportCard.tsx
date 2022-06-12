1f3ee26667588d9f5c9b891d890426f43818f26b
Péter Ferenc Gyarmati <dev.petergy@gmail.com>
2022-06-12 15:39:14 +0200

    feat: make `LanguageUsersReportCard` scrollable

--- apps/web/modules/reports/language-users/views/LanguageUsersReportCard.tsx
+++ apps/web/modules/reports/language-users/views/LanguageUsersReportCard.tsx
@@ -63,24 +63,26 @@ function LanguageUsersReportCard() {
             {queryResult?.data && (
               <>
                 {
-                  <div className="p-4 border-dashed border-black border-2 rounded-md flex justify-center items-start w-full">
+                  <div className="p-4 border-dashed border-black border-2 rounded-md flex justify-center items-start w-full max-h-[35vh] overflow-hidden">
                     {queryResult?.data.length === 0 && (
                       <p className="font-medium">No results were found</p>
                     )}
                     {queryResult?.data.length !== 0 && (
-                      <div className="flex flex-col overflow-auto">
+                      <div className="flex flex-col overflow-hidden">
                         <p className="font-medium">
                           The following email addresses were found:
                         </p>
-                        <div className="flex flex-col space-y-1.5">
-                          {[...queryResult.data, ''].map((email) => (
-                            <div
-                              key={email}
-                              className="border-gray-300 border-t-[0.5px] my-1 italic min-h-fit"
-                            >
-                              {email}
-                            </div>
-                          ))}
+                        <div className="flex flex-1 items-center justify-center overflow-y-scroll max-h-[25vh]">
+                          <div className="flex flex-col space-y-1.5 min-h-min">
+                            {[...queryResult.data, ''].map((email) => (
+                              <div
+                                key={email}
+                                className="border-gray-300 border-t-[0.5px] my-1 italic"
+                              >
+                                {email}
+                              </div>
+                            ))}
+                          </div>
                         </div>
                       </div>
                     )}
