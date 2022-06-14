c48c5cd7d513b5f0bdd16426ee7519f5bb0964c5
Péter Ferenc Gyarmati <dev.petergy@gmail.com>
2022-06-14 17:58:49 +0200

    feat: quick ui enhancements

--- apps/web/modules/snippet-navigator/views/SnippetDisplay.tsx
+++ apps/web/modules/snippet-navigator/views/SnippetDisplay.tsx
@@ -1,4 +1,4 @@
-import { Badge, Display, Loading, Snippet, Spacer, Text } from '@geist-ui/core';
+import { Badge, Display, Divider, Loading, Snippet, Spacer, Text, Tooltip } from "@geist-ui/core";
 import { useSnippetNavigatorState } from '../context/SnippetNavigatorContext';
 
 function SnippetDisplay() {
@@ -15,33 +15,45 @@ function SnippetDisplay() {
     <>
       <div className="w-full flex flex-col items-start justify-start space-y-5">
         {selectedTopic && (
-          <div className="flex space-x-2">
-            <p className="p-0 m-0 mr-2 font-bold text-navy-600">Topic Tags</p>
-            {selectedTopic.tags.map(({ name, color }) => (
-              <span
-                key={`${name}-${color}`}
-                className="px-2 py-1 border-2 rounded-xl uppercase text-sm"
-                style={{ borderColor: color }}
-              >
+          <div className="flex flex-col space-y-3">
+            <div className="flex space-x-4 justify-start items-center">
+              <div className="py-2 px-3 bg-navy-700 text-2xl text-center rounded-lg">
+                <p className="p-0 m-0 font-bold text-white uppercase">{selectedTopic.name}</p>
+              </div>
+              <div className="hidden lg:flex space-x-2">
+                {selectedTopic.tags.map(({ name, color }) => (
+                  <span
+                    key={`${name}-${color}`}
+                    className="px-2 py-1 border-2 rounded-xl capitalize text-sm"
+                    style={{ borderColor: color }}
+                  >
                 {name}
               </span>
-            ))}
+                ))}
+              </div>
+            </div>
+            <Text blockquote my={0} font="1rem">
+              {selectedTopic.description}
+            </Text>
           </div>
         )}
         {selectedSnippet && (
-          <div className="w-full flex-col grow">
-            <Text h2>{selectedSnippet.headline}</Text>
-            <Badge type="success">{`${langName} (v${langVersion})`}</Badge>
-            <Spacer h={2}></Spacer>
-            <Display shadow caption={createdAtString}>
-              <Snippet
-                text={selectedSnippet?.content}
-                symbol=""
-                type="lite"
-                className="pt-4"
-              />
-            </Display>
-          </div>
+          <>
+            <div className="w-full py-2 border-t-2 border-navy-900"></div>
+            <div className="w-full flex-col">
+              <Text h2>{selectedSnippet.headline}</Text>
+              <Badge type="success">{`${langName} (v${langVersion})`}</Badge>
+              <Spacer h={2}></Spacer>
+              <Display shadow caption={createdAtString}>
+                <Snippet
+                  text={selectedSnippet?.content}
+                  symbol=""
+                  type="lite"
+                  className="pt-4"
+                />
+              </Display>
+            </div>
+          </>
         )}
         {!selectedSnippet && <Loading>Waiting for a Snippet</Loading>}
       </div>
